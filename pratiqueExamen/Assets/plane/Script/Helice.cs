72ada10539e9dedbed94249ee04e91e1c8480a5a
B533P-28\533 <1641207@cegepba.qc.ca>
2020-10-05 12:36:43 -0400

    manque num 3+

---
+++ pratiqueExamen/Assets/plane/Script/Helice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helice : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") !=0)
+        {
+            /*Quaternion myRotation = this.transform.rotation;
+            this.transform.rotation = new Quaternion(myRotation.x,
+               myRotation.y,myRotation.z + 10,myRotation.w); Erreur ne sais pas pourquoi*/
+            this.transform.Rotate(Vector3.back);
+        }    
+    }
+}
---
+++ pratiqueExamen/Assets/plane/Script/Planemovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Planemovement : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    public float movementSpeed;
+    public float rotationSpeed;
+    public ParticleSystem smokeEffect;
+    public GameObject smoke;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = this.GetComponent<Rigidbody>();
+        movementSpeed = 1000;
+        rotationSpeed = 500;
+        smokeEffect.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") != 0)
+        {
+            if (!smokeEffect.isPlaying)
+            {
+                print("okays");
+                smokeEffect.Play(true);
+            }
+           // Destroy(Instantiate(smoke,this.transform),1);
+            myRigidbody.AddRelativeForce(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            smokeEffect.Stop();
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            myRigidbody.AddRelativeTorque(Vector3.right * Input.GetAxis("Vertical") * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            myRigidbody.AddRelativeTorque(Vector3.forward * Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime*-1);
+        }
+    }
+}
